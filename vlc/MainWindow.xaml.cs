10bbc82e96be561fa33fa050008adf5b9f73d083
Naserte sidementi <rokluk@seznam.cz>
2019-04-09 16:07:13 +0200

    init

--- vlc/MainWindow.xaml.cs
+++ vlc/MainWindow.xaml.cs
@@ -16,12 +16,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vlc.DotNet.Core;
 
 namespace vlc {
     /// <summary>
     /// Interakční logika pro MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public int a = 0; 
+        public int c = 0;
         public MainWindow() {
             InitializeComponent();
             var currentAssembly = Assembly.GetEntryAssembly();
@@ -38,7 +41,40 @@ namespace vlc {
 
             // Load libvlc libraries and initializes stuff. It is important that the options (if you want to pass any) and lib directory are given before calling this method.
             this.MyControl.SourceProvider.MediaPlayer.Play("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov");
+
+            //Event handler for 'current media time' label
+            this.MyControl.SourceProvider.MediaPlayer.PositionChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPositionChangedEventArgs>(MyControl_positionchanged);
+            //Event handler for setting trackBar1.Maximum on media load
+            //MyControl.SourceProvider.MediaPlayer.Playing += new System.EventHandler<VlcMediaPlayerPlayingEventArgs>(SetProgresMax);
+    }
+
+        private void MyControl_positionchanged(object sender, EventArgs e) {
+            zmenabaru();
+            //trackBar1.Value = pos;
         }
+
+        void zmenabaru() {
+            trackBar1.Value = this.MyControl.SourceProvider.MediaPlayer.Position;
+        }
+
+        // This is the main function which you looking.
+        private void trackBar1_Scroll(object sender, EventArgs e) {
+            //MyControl.SourceProvider.MediaPlayer.Time = trackBar1.Value * 1000;
+            int b = (int)MyControl.SourceProvider.MediaPlayer.Time / 1000;
+            int d = b / 60;
+            b = b - d * 60;
+            //label1.Text = d + ":" + b + "/" + c + ":" + a;
+            // The Time value is milisecond, you have divide 1000 for be second.
+        }
+
+        private void formdeneme_Load(object sender, EventArgs e) {
+            a = (int)MyControl.SourceProvider.MediaPlayer.Length / 1000;
+            trackBar1.Maximum = a;
+            c = a / 60;
+            a = a - c * 60;
+           // label1.Text = 0 + "/" + c + ":" + a;
+        }
+
 
         private void lod(object sender, RoutedEventArgs e) {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -58,5 +94,5 @@ namespace vlc {
         private void pse(object sender, RoutedEventArgs e) {
             this.MyControl.SourceProvider.MediaPlayer.Pause();
         }
-    }
+}
 }
